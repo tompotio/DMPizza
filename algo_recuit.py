9029c9e3bbf9ebc62e25e7e948a8fe6cc89adeba
tompotio <tomclem.ait@gmail.com>
2024-04-11 12:28:23 +0200

    ébauche de résolution du problème

--- algo_recuit.py
+++ algo_recuit.py
@@ -31,36 +31,74 @@ class Ingredients:
         return [ingredient for ingredient, _ in ordre_decroissant[:n]]
     
 class RecuitSimule:
-    def __init__(self,solution,temperature,ingredients) -> None:
-        self.solution = solution
-        self.temperature = temperature
-        self.ingredients = ingredients
 
-    def generateNeighbors(seq):
+    def __init__(self,solution,ingredients) -> None:
+        self.solution = solution
+        self.solutionCourante = solution
+        self.ingredients = ingredients
+        self.nb_clients_sol_courante = self.evaluation_solution(solution)
+        self.Imax = len(ingredients.ingredients)
+
+        # Paramètres variants.
+        self.T0 = self.Imax * 2
+        self.T = self.T0
+        self.Tmin = self.Imax
+        self.tau = 1e4
+        self.beta = 1e-4
+
+    def generateNeighbor(self):
+        seq = self.solutionCourante
         length = len(seq)
-        start = rd.randint(0, length - 2)  # Point de départ aléatoire
-        end = rd.randint(start+1, length - 1)
+        start = 0
+        end = length
+        if (length >= 3):
+            start = rd.randint(0, length - 2)  # Point de départ aléatoire
+            end = rd.randint(start+1, length - 1)
         seq[start:end + 1] = reversed(seq[start:end + 1])
         return seq
     
-    def find_optimal_solution():
-        while T > Tmin:
-            solution_candidate = mouvement()
-            nb_clients_sol_candidate = evaluation_solution(solution_candidate)
+    # Calcul de la fonction d'évalution de la solution donnée.
+    def evaluation_solution(self, solution):
+        n = N # Initialise un petit n qui prend le nombre de clients.
 
-            if nb_clients_sol_courante < nb_clients_sol_candidate:
-                nb_clients_sol_courante = nb_clients_sol_candidate
-            else: 
-                dE = nb_clients_sol_courante - nb_clients_sol_candidate
-                
+        for client in clients:
+            nbi = 0 # Nombre d'ingredients que le client aime, dans la solution.
+            ok = True
+            for ingredient in solution:
+                if (ingredient in client.ingredient_aimes):
+                    nbi +=1
+                if (ingredient in client.ingredient_detestes): 
+                    ok = False
+            # On vérifie que le client a tous les ingredients qu'il aime et aucun qu'il n'aime pas.
+            if (not(ok and (nbi == client.nb_aimes))):
+                n-=1 # Sinon on retire le client.
 
-            # Décroissement de la température suivant une loi exponentielle.
-            T = T0 * np.exp(-t/tau)
+        return n
+    
+    def find_optimal_solution(self):
+        i = 0
+        while self.T > self.Tmin:
+            solution_candidate = self.generateNeighbor()
+            nb_clients_sol_candidate = self.evaluation_solution(solution_candidate)
+
+            if self.nb_clients_sol_courante < nb_clients_sol_candidate:
+                self.nb_clients_sol_courante = nb_clients_sol_candidate
+                self.solutionCourante = solution_candidate
+            else:
+                dE = self.nb_clients_sol_courante - nb_clients_sol_candidate
+                if self.beta < np.exp(-dE/self.T):
+                    self.solutionCourante = solution_candidate
+                    self.nb_clients_sol_courante = nb_clients_sol_candidate
+
+            # Abaissement de la température suivant une loi exponentielle.
+            self.T = self.T0 * np.exp(-i/self.tau)
+            i+=1
+        return self.solutionCourante
     
     def is_accepted(Xca,Xco,temperature):
-        proba = exp((Xca-Xco)/temperature)
+        proba = np.exp((Xca-Xco)/temperature)
         test = rd.uniform(0.0,1.0)
-        return proba>test
+        return proba > test
 
 N = 0 # Nombre de clients
 Imax = 0 # Nombre d'ingredients
@@ -112,63 +150,25 @@ with open(filename,'r') as fichier:
         #print("Ingrédients aimés : ",ingredients_aimes)
         #print("Ingredients mal aimés : ", ingredients_detestes)
 
-Imax = len(ingredients.ingredients)
-
-# Calcul de la fonction d'évalution de la solution donnée.
-def evaluation_solution(solution):
-    n = N # Initialise un petit n qui prend le nombre de clients.
-
-    for client in clients:
-        nbi = 0 # Nombre d'ingredients que le client aime, dans la solution.
-        ok = True
-        for ingredient in solution:
-            if (ingredient in client.ingredient_aimes):
-                nbi +=1
-            if (ingredient in client.ingredient_detestes): 
-                ok = False 
-        # On vérifie que le client a tous les ingredients qu'il aime et aucun qu'il n'aime pas.
-        if (not(ok and (nbi == client.nb_aimes))):
-            n-=1 # Sinon on retire le client.
-
-    return n
-
 def taille_solution_initiale():
-    p = np.linspace(1, 0, Imax, dtype=float)
+    p = np.linspace(1, 0, len(ingredients.ingredients), dtype=float)
     p = p / np.sum(p)
-    return np.random.choice(range(1, Imax + 1), p=p)
-
-def exp(delta, T):
-    return math.exp(-delta/T)
-
-def mouvement():
-    return
+    return np.random.choice(range(1, len(ingredients.ingredients) + 1), p=p)
 
 # Génération d'une solution initiale.
 # On demande à l'objet de la classe Ingredients, de renvoyer les ingredients,
 # les plus appréciés. 
 # On demande un nombre 
 solution_initiale = ingredients.get_meilleurs_ingredients(taille_solution_initiale())
-solution_courante = solution_initiale
-nb_clients_sol_courante = evaluation_solution(solution_courante)
-
-print(solution_initiale)
-
-# Paramètres variants.
-T0 = Imax * 2
-T = T0
-Tmin = 1e-1
-tau = 1e4
-beta = 1e-4
-t = 0
 
 # Algorithme du recuit simulé.
-algo_recuit = RecuitSimule()
+algo_recuit = RecuitSimule(solution_initiale,ingredients)
+sol = algo_recuit.find_optimal_solution()
+nb =0
+while((algo_recuit.nb_clients_sol_courante < 5) and (nb < 1000)):
+    sol = algo_recuit.find_optimal_solution()
+    nb+=1
 
-
-
-
-
-
-
-
-
+print("nb = ", nb)
+print("solution = ",sol)
+print("val sol =",algo_recuit.nb_clients_sol_courante)
